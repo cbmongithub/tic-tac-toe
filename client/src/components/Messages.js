8406a6ce6e1f20118d7317644bcef52ef0f97353
Christian Martinez <christian@mixdlabs.com>
2023-06-10 10:47:06 -0600

    We will figure out scroll to bottom

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -2,14 +2,15 @@ import ScrollToBottom from 'react-scroll-to-bottom'
 
 import Message from './Message'
 
-const Messages = ({ messages, name }) => (
-  <ScrollToBottom className='messages'>
-    {messages.map((message, i) => (
-      <div key={i}>
-        <Message message={message} name={name} />
-      </div>
-    ))}
-  </ScrollToBottom>
-)
+const Messages = ({ isSSR, messages, name }) =>
+  !isSSR && (
+    <ScrollToBottom className='messages'>
+      {messages.map((message, i) => (
+        <div key={i}>
+          <Message message={message} name={name} />
+        </div>
+      ))}
+    </ScrollToBottom>
+  )
 
 export default Messages
